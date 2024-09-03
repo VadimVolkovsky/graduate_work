464298dfe516efdfbe9aae941b1e7266fdce0f8c
VadimVolkovsky <vadimas29@gmail.com>
2024-09-03 17:38:35 +0300

    добавлен конфиг minio

--- workers/src/core/config.py
+++ workers/src/core/config.py
@@ -14,8 +14,15 @@ class Settings(BaseSettings):
     rabbit_user: str = "guest"
     rabbit_password: str = "guest"
 
+    minio_host: str = 'localhost'
+    minio_port: int = 9000
+    minio_root_user: str = 'minio_user'
+    minio_root_password: str = 'minio_password'
+    minio_secure: bool = False
+    minio_bucket_name: str = 'graduate-work-bucket'
+
     class Config:
-        env_file = '.env'
+        # env_file = '.env'
         extra = 'ignore'
 
 
