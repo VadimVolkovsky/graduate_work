96ba833b2c7e34958a6a0bd29470c5dda976ebf1
VadimVolkovsky <vadimas29@gmail.com>
2024-09-11 22:50:06 +0300

    алхимия для воркера

--- workers/src/models/entity.py
+++ workers/src/models/entity.py
@@ -1,6 +1,8 @@
 from sqlalchemy import create_engine, Column, String, UUID, DateTime
 from sqlalchemy.orm import declarative_base
+
 from workers.src.core.config import settings
+
 # from core.config import settings
 
 Base = declarative_base()
