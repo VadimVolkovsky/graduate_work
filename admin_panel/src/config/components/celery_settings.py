e00817ac6aef6e9734e81c80a812748ff7977abc
Yurii Kolpakov <yurii.kolpakov@newstream.ai>
2024-09-12 22:44:28 +0300

    обновлена конфигурация django

--- admin_panel/src/config/components/celery_settings.py
+++ admin_panel/src/config/components/celery_settings.py
@@ -1,17 +1,42 @@
-# CELERY
-CELERY_BROKER_URL = 'redis://redis:6379/0'
-CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
+import os
 
-# CELERY_TASK_TRACK_STARTED = True  # запускает трекинг задач Celery
+from kombu import Exchange, Queue
 
-# Планировщик задач
-CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
+CELERY_BROKER_URL = 'amqp://guest:guest@rabbit:5672//'  # URL для RabbitMQ
+CELERY_RESULT_BACKEND = 'django-db'  # Если используете django-celery-results
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'UTC'
 
-# CELERY_BROKER_TRANSPORT_OPTION = {'visibility_timeout': 3600}  # время ожидания видимости 1 час
-CELERY_RESULT_BACKEND = 'django-db'  # указание для django_celery_results куда записывать результат выполнения задач
-# CELERY_ACCEPT_CONTENT = ['application/json']  # это тип содержимого, разрешенный к получению
-# CELERY_TASK_SERIALIZER = 'json'  # это строка, используемая для определения метода сериализации по умолчанию
-# CELERY_RESULT_SERIALIZER = 'json'  # является типом формата сериализации результатов
+# # Определите обменник
+# default_exchange = Exchange('default', type='direct')
 #
-# CELERY_TASK_DEFAULT_QUEUE = 'default'  # celery будет использовать это имя очереди
-CELERY_IMPORTS = ("notification.tasks", )
+# # Определите очередь
+# new_video_queue = Queue('new_video', exchange=default_exchange, routing_key='new_video')
+#
+# CELERY_QUEUES = (
+#     new_video_queue,
+# )
+#
+# CELERY_DEFAULT_EXCHANGE = 'default_exchange'
+# CELERY_DEFAULT_EXCHANGE_TYPE = 'direct'
+# CELERY_DEFAULT_ROUTING_KEY = 'new_video'
+
+# CELERY_QUEUES = {
+#     'default': {
+#         'exchange': 'default',
+#         'exchange_type': 'direct',
+#         'binding_key': 'default',
+#     },
+#     'new_video': {
+#         'exchange': 'default_exchange',
+#         'exchange_type': 'direct',
+#         'binding_key': 'new_video',
+#     },
+# }
+
+RABBITMQ_HOST = os.environ.get('RABBIT_HOST', '127.0.0.1')
+RABBITMQ_PORT = os.environ.get('RABBIT_PORT', '5672')
+RABBITMQ_USER = os.environ.get('RABBIT_USER', '')
+RABBITMQ_PASSWORD = os.environ.get('RABBIT_PASSWORD', '')
